edd1a2d206e906ed088900e0b2bf8c113548420b
Anton Berkutov <void__fastcall@mail.ru>
2025-03-01 17:54:51 +0300

    add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function () 
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = { "ruby", "lua", "vim", "vimdoc", "javascript", "html", "css", "scss", "sql", "go", "slim", "devicetree" },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },  
+    })
+  end
+}
