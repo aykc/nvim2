ef9f76c776b1bdbb6003f4c678144f4437be2a6d
Anton Berkutov <void__fastcall@mail.ru>
2025-03-02 19:50:28 +0300

    add highlight colors

---
+++ lua/plugins/nvim-highlight-colors.lua
@@ -0,0 +1,10 @@
+return {
+  "brenoprata10/nvim-highlight-colors",
+  opts = {
+    -- render = "virtual",
+    -- virtual_symbol_position = 'eow',
+    virtual_symbol_suffix = '',
+    enable_named_colors = false,
+    enable_tailwind = true,
+  },
+}
