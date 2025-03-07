aa0a1b7a7c1e632f553602ff8095b731d09238a0
Anton Berkutov <void__fastcall@mail.ru>
2025-03-08 01:20:50 +0300

    add completions

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,16 +1,26 @@
 return {
 	"neovim/nvim-lspconfig",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	lazy = false,
 	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 		local lspconfig = require("lspconfig")
-		lspconfig.lua_ls.setup({})
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+		})
 		lspconfig.ruby_lsp.setup({
+			capabilities = capabilities,
 			mason = false,
 			cmd = { "ruby-lsp" },
 			-- cmd = { vim.fn.expand("~/.rbenv/versions/3.1.2/bin/ruby-lsp") },
 		})
 		-- lspconfig.solargraph.setup {}
 		-- lspconfig.rubocop.setup {}
-    lspconfig.ts_ls.setup({})
+		lspconfig.ts_ls.setup({
+			capabilities = capabilities,
+		})
 
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
