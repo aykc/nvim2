dcc03d986f2d34a51578cef6763de32a40bfb756
Anton Berkutov <void__fastcall@mail.ru>
2025-03-18 23:51:26 +0300

    change indent appearance

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,7 +1,7 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
-	--[[ opts = function(_, opts)
+	opts = function(_, opts)
 		opts = {
 			indent = {
 				-- highlight = highlight,
@@ -21,8 +21,8 @@ return {
 	end,
 	dependencies = {
 		"TheGLander/indent-rainbowline.nvim",
-	}, ]]
-	config = function()
+	},
+	--[[ config = function()
 		local highlight = {
 			"RainbowRed",
 			"RainbowYellow",
@@ -58,5 +58,5 @@ return {
 		end)
 
 		require("ibl").setup(opts)
-	end,
+	end, ]]
 }
